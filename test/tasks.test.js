e15d07a38d1539f9383378f13d8cd8a6b4af214e
QuentinB <quentin.bourdois@supdevinci-edu.fr>
2025-10-01 15:02:25 +0200

    Update tasks.test.js to try and conflict on merge

--- test/tasks.test.js
+++ test/tasks.test.js
@@ -15,8 +15,8 @@ test('addTask adds a task correctly', () => {
 });
 
 test('addTask trims whitespace from task name', () => {
-  const task = addTask('   Trimmed task   ');
-  expect(task.name).toBe('Trimmed task');
+  const task = addTask('   Trimmed task noConflict   ');
+  expect(task.name).toBe('Trimmed task noConflict');
 });
 
 test('addTask throws error for invalid names', () => {
