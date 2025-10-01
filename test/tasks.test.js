b5a414c07608965e6c08bb0a8597431246448c0d
QuentinB <quentin.bourdois@supdevinci-edu.fr>
2025-10-01 14:46:26 +0200

    Add tests for toggleTask and countDone functions in tasks.test.js

--- test/tasks.test.js
+++ test/tasks.test.js
@@ -1,4 +1,4 @@
-const { getTasks, reset, addTask } = require('../lib/tasks');
+const { getTasks, reset, addTask, toggleTask, countDone } = require('../lib/tasks');
 
 beforeEach(() => {
   reset();
@@ -23,4 +23,27 @@ test('addTask throws error for invalid names', () => {
   expect(() => addTask('')).toThrow('Invalid task name');
   expect(() => addTask('   ')).toThrow('Invalid task name');
   expect(() => addTask(123)).toThrow('Invalid task name');
+});
+
+test('toggleTask toggles the done status', () => {
+  const { toggleTask } = require('../lib/tasks');
+  const task = addTask('Toggle me');
+  expect(task.done).toBe(false);
+  const toggled = toggleTask(task.id);
+  expect(toggled.done).toBe(true);
+  const toggledBack = toggleTask(task.id);
+  expect(toggledBack.done).toBe(false);
+});
+
+test('toggleTask throws error for invalid id', () => {
+  const { toggleTask } = require('../lib/tasks');
+  expect(() => toggleTask(999)).toThrow('Task not found');
+});
+
+test('countDone returns correct number', () => {
+  const { toggleTask, countDone } = require('../lib/tasks');
+  addTask('Task 1');
+  const t2 = addTask('Task 2');
+  toggleTask(t2.id);
+  expect(countDone()).toBe(1);
 });
